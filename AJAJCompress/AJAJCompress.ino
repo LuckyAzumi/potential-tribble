09c611fe7824eb288ab2983256b968562d450158
LuckyAzumi <luckyazumi@icloud.com>
2018-09-02 17:02:59 -0500

    Code 0.0.2 (Metal detector added)

--- AJAJCompress/AJAJCompress.ino
+++ AJAJCompress/AJAJCompress.ino
@@ -7,9 +7,18 @@ const int ser1 = 7, ser2 = 8; //The digital pin where the servos are going to co
 Servo servo1; 
 Servo servo2;
 
+bool plastic; //If the item isn't a plastic, the program will continue
+bool metal; //If the item isn't a metal, the program will continue
+
 const int ir_send1 = 2,ir_send2 = 3;  //The position of the IR LED on the Digital pin
 const int ir_rec1 = A0, ir_rec2 = A1; //The position of the IR Receiver on the Analog Pin
 int irSensorValue1 = 0, irSensorValue2 = 0; //The values received from the IR Receiver
+
+const int metalDetection = A2;  //The position of the metal detector on the analog pin
+float metalDetected;  //The value that shows the proximity of the nearest metal
+int monitoring; // The variable that receives the data from the detector
+
+
 
 void setup() {
   
@@ -26,12 +35,46 @@ void loop() {
   irSensorValue2 = analogRead(ir_rec2);
   //Reads the value from the receiver and stores it in the respective variable
   
-  if((irSensorValue1==LOW)&&(irSensorValue2==LOW)){ 
+  if(((irSensorValue1==LOW)&&(irSensorValue2==LOW))||((irSensorValue1==LOW)||(irSensorValue2==LOW))){ 
    //Checks if the values are low/non-existant
 
    Serial.println("Sensor receiver obstructed, meaning the item isn't a plastic.");
    //Prints a debug message on the Serial
+
+   plastic=false;
+   //Tells that the item isn't a plastic
+  }
+  
+  else if ((irSensorValue1==HIGH)&&(irSensorValue2==HIGH)){
+    //Checks if the values are high/existing
+
+    Serial.println("Item is a plastic.");
+    //Prints a debug message on the Serial
+    
+    plastic=true;
+    //Tells that the item is a plastic
+  }
    
+  if (plastic==false){
+    monitoring = analogRead(metalDetection);
+    metalDetected = (float) monitoring*100/1024.0;
+    Serial.print("Initializing Proximity Sensor");
+    delay(500);
+    Serial.print("Please wait...");
+    delay(1000);
+    Serial.print("Metal is Proximited = ");
+    Serial.print(metalDetected);
+    Serial.println("%");
+    if (monitoring > 250){
+    Serial.println("Metal is Detected");
+    metal=true;
+    }
+    delay(1000);
+  }
+}
+
+
+void moveServoOne_90(){
    servo1.attach(ser1); //Connects the servo
    for(int i=0;i<=servopos1;i++){
     servo1.write(i);  //Writes the servo position 1 by 1, until it gets to 90 degrees
@@ -42,14 +85,5 @@ void loop() {
    servo1.write(0);
    servo1.detach();
    //Waits a second before going back to the position 0 and disconnecting the servo
-  }
-  
-  else if (((irSensorValue1==HIGH)&&(irSensorValue2==HIGH))||((irSensorValue1==HIGH)||(irSensorValue2==HIGH))){
-    //Checks if the values are high/existing
-
-    Serial.println("Item isn't a plastic (or at least, it isn't one that allows the passing of IR light).");
-    //Prints a debug message on the Serial
-  }
-   
-  
 }
+
